3a42b567bdd0298ea16f9ceb34fca63380e87330
Vaansh Sanghrajka <vaansh@mac.mynetworksettings.com>
2026-06-23 12:20:22 -0400

    Add base functions

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static int calculateSum(int[] numbers) {
+        int totalSum = 0;
+        for (int number : numbers) {
+            totalSum += number;
+        }
+        return totalSum;
+    }
+
+    public static int findMax(int[] numbers) {
+        int max = numbers[0];
+        for (int n : numbers) {
+            if (n > max) max = n;
+        }
+        return max;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 2) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
