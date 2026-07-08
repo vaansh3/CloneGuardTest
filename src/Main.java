ae111fde174c36e5daf511f7b8558edcfd10f0e1
Vaansh Sanghrajka <vaansh@mac.mynetworksettings.com>
2026-07-08 18:44:09 -0400

    Add missing helper method to complete the refactor

--- src/Main.java
+++ src/Main.java
@@ -24,14 +24,22 @@ public class Main {
         return true;
     }
 
-public int sumArray(int[] arr) {
-    return coreSumArraySafe(arr);
-}
+    public int sumArray(int[] arr) {
+        return coreSumArraySafe(arr);
+    }
 
-public int sumArraySafe(int[] arr) {
-    if (arr == null) {
+    public int sumArraySafe(int[] arr) {
+        if (arr == null) {
             return 0;
         }
-    return coreSumArraySafe(arr);
-}
+        return coreSumArraySafe(arr);
+    }
+
+    private static int coreSumArraySafe(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
+    }
 }
