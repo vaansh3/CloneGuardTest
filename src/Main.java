0e5daf33dde11294cc64e990abaeaff40e4f6f04
vaansh3 <vaansh3@gmail.com>
2026-07-08 20:47:12 -0400

    Update src/Main.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -24,19 +24,21 @@ public class Main {
         return true;
     }
 
-    public int multiplyAll(int[] arr) {
-        int product = 1;
-        for (int i = 0; i < arr.length; i++) {
-            product *= arr[i];
-        }
-        return product;
-    }
+public int multiplyAll(int[] arr) {
+    return coreMultiplyAllExact(arr);
+}
 
-    public int multiplyAllExact(int[] arr) {
-        int product = 1;
-        for (int i = 0; i < arr.length; i++) {
+
+
+public int multiplyAllExact(int[] arr) {
+    return coreMultiplyAllExact(arr);
+}
+
+private static int coreMultiplyAllExact(int[] arr) {
+    int product = 1;
+    for (int i = 0; i < arr.length; i++) {
             product *= arr[i];
         }
-        return product;
-    }
+    return product;
+}
 }
