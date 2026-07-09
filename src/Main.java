f60f400bc9897f9af81b39039017a4420439fc0e
Vaansh Sanghrajka <vaansh@mac.mynetworksettings.com>
2026-07-09 12:49:42 -0400

    Add all four clone types in one file for combined test

--- src/Main.java
+++ src/Main.java
@@ -23,4 +23,70 @@ public class Main {
         }
         return true;
     }
+
+    public int multiplyAll(int[] arr) {
+        int product = 1;
+        for (int i = 0; i < arr.length; i++) {
+            product *= arr[i];
+        }
+        return product;
+    }
+
+    public int multiplyAllExact(int[] arr) {
+        int product = 1;
+        for (int i = 0; i < arr.length; i++) {
+            product *= arr[i];
+        }
+        return product;
+    }
+
+    public int sumPositives(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) total += arr[i];
+        }
+        return total;
+    }
+
+    public int sumPositiveValues(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) result += nums[i];
+        }
+        return result;
+    }
+
+    public int sumArray(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
+    }
+
+    public int sumArraySafe(int[] arr) {
+        if (arr == null) {
+            return 0;
+        }
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
+    }
+
+    public int factorialIterative(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public int factorialRecursive(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorialRecursive(n - 1);
+    }
 }
