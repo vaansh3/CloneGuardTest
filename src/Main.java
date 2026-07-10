07e1037f05667ee77f36d12546361d5aa7457d32
Vaansh Sanghrajka <vaansh@mac.mynetworksettings.com>
2026-07-10 11:24:53 -0400

    Test conflict detection: two groups sharing multiplyAll

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 public class Main {
 
-    public static int calculateSum(int[] numbers) {
-        int totalSum = 0;
-        for (int number : numbers) {
-            totalSum += number;
+    public int multiplyAll(int[] arr) {
+        int product = 1;
+        for (int i = 0; i < arr.length; i++) {
+            product *= arr[i];
         }
-        return totalSum;
+        return product;
     }
 
-    public static int findMax(int[] numbers) {
-        int max = numbers[0];
-        for (int n : numbers) {
-            if (n > max) max = n;
+    public int multiplyAllExact(int[] arr) {
+        int product = 1;
+        for (int i = 0; i < arr.length; i++) {
+            product *= arr[i];
         }
-        return max;
+        return product;
     }
 
-    public static boolean isPrime(int number) {
-        if (number < 2) return false;
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
+    public int multiplyAllRecursive(int[] arr) {
+        if (arr.length == 0) {
+            return 1;
         }
-        return true;
+        int[] rest = java.util.Arrays.copyOfRange(arr, 1, arr.length);
+        return arr[0] * multiplyAllRecursive(rest);
     }
 }
