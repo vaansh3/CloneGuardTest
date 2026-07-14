b70f5dd578f63367c6a047c001847e312652cfe4
Vaansh Sanghrajka <vaansh@mac.mynetworksettings.com>
2026-07-14 16:01:49 -0400

    Scenario 3 round 5: single Type 2 clone only

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,18 @@
 public class Main {
 
-    public static int calculateSum(int[] numbers) {
-        int totalSum = 0;
-        for (int number : numbers) {
-            totalSum += number;
+    public int countEven(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) count++;
         }
-        return totalSum;
+        return count;
     }
 
-    public static int findMax(int[] numbers) {
-        int max = numbers[0];
-        for (int n : numbers) {
-            if (n > max) max = n;
+    public int countEvenValues(int[] values) {
+        int result = 0;
+        for (int j = 0; j < values.length; j++) {
+            if (values[j] % 2 == 0) result++;
         }
-        return max;
-    }
-
-    public static boolean isPrime(int number) {
-        if (number < 2) return false;
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
-        }
-        return true;
+        return result;
     }
 }
